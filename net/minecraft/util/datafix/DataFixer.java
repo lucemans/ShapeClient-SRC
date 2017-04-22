79d5402d1123009502342616a540fd36501935e7
lucemans <mrdisk@hotmail.com>
2017-04-22 11:30:48 +0200

    Add files via upload

--- net/minecraft/util/datafix/DataFixer.java
+++ net/minecraft/util/datafix/DataFixer.java
@@ -24,7 +24,7 @@ public class DataFixer implements IDataFixer
     public NBTTagCompound process(IFixType type, NBTTagCompound compound)
     {
         int i = compound.hasKey("DataVersion", 99) ? compound.getInteger("DataVersion") : -1;
-        return i >= 819 ? compound : this.process(type, compound, i);
+        return i >= 922 ? compound : this.process(type, compound, i);
     }
 
     public NBTTagCompound process(IFixType type, NBTTagCompound compound, int versionIn)
